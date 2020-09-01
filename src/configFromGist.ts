d435adbe6966fa7d8b55010dd3a33ed965c82ef5
Balthasar Hofer <lebalz@outlook.com>
2020-09-01 19:22:17 +0200

    update to fetch configs and pips from gists

---
+++ src/configFromGist.ts
@@ -0,0 +1,61 @@
+import * as vscode from "vscode";
+import { default as axios } from "axios";
+import * as _ from "lodash";
+
+function vscodeConfigFromGist(): Promise<Object> {
+  const config = vscode.workspace.getConfiguration();
+  const gistUrl = config.get("gbsl.gistConfigurationUrl");
+  if (!gistUrl || gistUrl === "") {
+    return new Promise((resolve) => resolve({}));
+  }
+  return axios
+    .get(`${gistUrl}/raw`, { responseType: "json" })
+    .then((data) => {
+      if (typeof data.data !== "object") {
+        throw new Error("Invalid JSON in configuration");
+      }
+      return data.data;
+    })
+    .catch((error) => {
+      vscode.window.showErrorMessage(
+        `Gist Content could not be downloaded from ${gistUrl}: ${error}`
+      );
+      return {};
+    });
+}
+
+export function setGistConfig(): Promise<
+  {
+    name: string;
+    value: any;
+    updated: boolean;
+  }[]
+> {
+  return vscodeConfigFromGist().then((config) => {
+    const configuration = vscode.workspace.getConfiguration();
+    const toUpdate = Object.entries(config).filter(([key, value]) => {
+      if (!configuration.has(key)) {
+        return true;
+      }
+      const sanitizedCurrent = JSON.parse(
+        JSON.stringify(configuration.get(key))
+      );
+      const sanitizedNew = JSON.parse(JSON.stringify(value));
+      if (!_.isEqual(sanitizedCurrent, sanitizedNew)) {
+        return true;
+      }
+      return false;
+    });
+    return Promise.all(
+      toUpdate.map(async ([key, value]) => {
+        try {
+          return await configuration
+            .update(key as any, value, vscode.ConfigurationTarget.Global)
+            .then(() => ({ name: key, value: value, updated: true }));
+        } catch (error) {
+          return { name: key, value: value, updated: false };
+        }
+      })
+    );
+  });
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,153 +1,77 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { join } from "path";
-import { readFileSync } from "fs";
+import { setGistConfig } from "./configFromGist";
 
-interface PipPackage {
-  package: string;
-  version: string;
-}
-
-const PIP_PACKAGES: PipPackage[] = [
-  { package: "pytest", version: "6.0.1" },
-  { package: "matplotlib", version: "3.3.1" },
-  { package: "ipython", version: "7.17.0" },
-  { package: "jupyter", version: "1.0.0" },
-  { package: "numpy", version: "1.19.1" },
-  { package: "scipy", version: "1.5.2" },
-  { package: "pandas", version: "1.1.0" },
-  { package: "openpyxl", version: "3.0.4" },
-  { package: "xlrd", version: "1.2.0" },
-];
-
-const DEFAULT_USER_SETTINGS = {
-  "workbench.colorTheme": "One Dark Pro",
-  "editor.suggestSelection": "first",
-  "vsintellicode.modify.editor.suggestSelection":
-    "automaticallyOverrodeDefaultValue",
-  "keyboard.dispatch": "keyCode",
-  "editor.mouseWheelZoom": true,
-  "python.languageServer": "Microsoft",
-  "workbench.activityBar.visible": true,
-  "python.linting.pylintEnabled": true,
-  "python.linting.enabled": true,
-  "python.dataScience.alwaysTrustNotebooks": true,
-  "python.dataScience.askForKernelRestart": false,
-  "python.dataScience.stopOnFirstLineWhileDebugging": false,
-  "python.formatting.autopep8Args": ["--select=E,W", "--max-line-length=120"],
-  "editor.minimap.enabled": false,
-  "workbench.settings.useSplitJSON": true,
-  "python.linting.pylintArgs": ["--load-plugin", "pylint_protobuf"],
-  "pythonTestExplorer.testFramework": "pytest",
-};
-
-function setConfig() {
+function configure(
+  force: boolean = false,
+  silent: boolean = false
+): Promise<number> {
   const configuration = vscode.workspace.getConfiguration();
-  return Object.entries(DEFAULT_USER_SETTINGS).map(async ([key, value]) => {
-    try {
-      return await configuration.update(
-        key as any,
-        value,
-        vscode.ConfigurationTarget.Global
-      );
-    } catch (error) {
-      return await setTimeout(() => {}, 0);
-    }
-  });
-}
-
-function configure() {
-  const configuration = vscode.workspace.getConfiguration();
-  const skip = configuration.get("ispw.ignore_configurations", false);
+  const skip = configuration.get("ispw.ignoreConfigurations", false);
   if (skip) {
-    vscode.window.showInformationMessage(
-      "ISPW Configuration is ignored. Edit your settings"
-    );
+    if (!silent) {
+      vscode.window.showInformationMessage(
+        "ISPW Configuration is ignored. Edit your settings"
+      );
+    }
     return new Promise((resolve) => resolve());
   }
   vscode.window.showInformationMessage(`Configure vs code`);
-  return Promise.all(setConfig()).then(() => {
-    vscode.window.showInformationMessage(`configuration done`);
+  return setGistConfig().then((settings) => {
+    const successful = settings.filter(
+      (s) => s.updated && s.name !== "files.exclude"
+    );
+    const err = settings.filter((s) => !s.updated);
+    const errMsg =
+      err.length < 5
+        ? `could not update ${err.map((s) => s.name).join(", ")}`
+        : `could not update ${err.length} settings`;
+    if (successful.length > 5) {
+      vscode.window.showInformationMessage(
+        `configured ${successful.length} settings${
+          err.length > 0 ? errMsg : ""
+        }`
+      );
+    } else if (successful.length > 0) {
+      vscode.window.showInformationMessage(
+        `configured ${successful.map((s) => s.name).join(", ")} ${
+          err.length > 0 ? errMsg : ""
+        }`
+      );
+    } else if (err.length > 0) {
+      vscode.window.showWarningMessage(errMsg);
+    }
+    return successful.length;
   });
-}
-
-function extensionVersion(context: vscode.ExtensionContext) {
-  var extensionPath = join(context.extensionPath, "package.json");
-  var packageFile = JSON.parse(readFileSync(extensionPath, "utf8"));
-
-  return packageFile?.version ?? "0.0.1";
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   const configuration = vscode.workspace.getConfiguration();
-  vscode.commands
-    .executeCommand("python2go.isPythonInstalled")
-    .then((isInstalled) => {
-      if (isInstalled) {
-        vscode.commands
-          .executeCommand("python2go.pipPackages")
-          .then((pkgs: any) => {
-            const packages = pkgs as PipPackage[];
-            const toUninstall = PIP_PACKAGES.filter((pkg) =>
-              packages.some(
-                (installed) =>
-                  installed.package === pkg.package &&
-                  installed.version !== pkg.version
-              )
-            );
-            console.log(toUninstall);
-            return new Promise((resolve) => {
-              if (toUninstall.length === 0) {
-                return resolve(true);
-              }
-              return resolve(
-                vscode.commands.executeCommand(
-                  "python2go.pip",
-                  `uninstall -y ${toUninstall.map((p) => p.package).join(" ")}`
-                )
-              );
-            }).then(() => {
-              const toInstall = PIP_PACKAGES.filter(
-                (pkg) =>
-                  !packages.some(
-                    (installed) => installed.package === pkg.package && installed.version === pkg.version
-                  )
-              );
-              if (toInstall.length > 0) {
-                const target = process.platform === "win32" ? "--user" : "";
-                const toInstallPkgs = toInstall.map((pkg) => `${pkg.package}==${pkg.version}`);
-                return vscode.commands.executeCommand(
-                  "python2go.pip",
-                  `install ${target} ${toInstallPkgs.join(" ")}`
-                );
-              }
-              return new Promise((resolve) => resolve());
-            });
-          })
-          .then(() => {
-            if (configuration.get("ispw.ignore_configurations", false)) {
-              return;
-            }
-            const configVersion = context.globalState.get("configVersion");
-            const pluginVersion = extensionVersion(context);
-            if (configVersion !== pluginVersion) {
-              return configure().then(() => {
-                context.globalState.update("configVersion", pluginVersion);
-              });
-            }
-          });
-      }
-    });
+
+  if (!configuration.get("ispw.ignoreConfigurations", false)) {
+    configure(false, true);
+  }
 
   let configureDisposer = vscode.commands.registerCommand(
     "ispw.configure",
     () => {
-      return configure().then(() => {
-        vscode.window.showInformationMessage("Configured ISPW settings");
-      });
+      return vscode.commands
+        .executeCommand("python2go.configure")
+        .then(
+          () => {
+            return configure(true);
+          },
+          /** configure ispw settings anyway */
+          () => {
+            return configure(true);
+          }
+        )
+        .then(() => {
+          vscode.window.showInformationMessage("Configured ISPW settings");
+        });
     }
   );
   context.subscriptions.push(configureDisposer);
